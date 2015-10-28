193d070d5e9136a2343cc1239aa1f1187be27a82
tungpt247 <tungpt247@gmail.com>
2015-10-28 17:35:36 +0700

    API availability checking in Swift 2.1

---
+++ api-availability-cheking.playground/Contents.swift
@@ -0,0 +1,12 @@
+/*:
+# API Availability checking
+
+use #available()” block. we give a comma separad list of platforms and version numbers
+pattern: "platform version" ex: iOS 0.9, OSX 10.9
+*/
+
+if #available(iOS 9.0, OSX 10.9, *) {
+  
+}
+
+

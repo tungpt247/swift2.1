6824f6df3b1cba5ded896409e1046971f3e9fefb
tungpt247 <tungpt247@gmail.com>
2015-10-28 17:25:18 +0700

    Error handling in Swift 2.1

--- Error-Handling.playground/Contents.swift
+++ Error-Handling.playground/Contents.swift
@@ -1,8 +1,39 @@
-//: Playground - noun: a place where people can play
+/*:
+# Error handling
 
-enum VendingMachineError: ErrorType {
-  case InvalidParam
-  case InsufficientFund(type: Int)
-  case OutOfStack
+If you want to thw an error, the object thrown must conform to the ErrorType protocol
+As you haved expected, NSError conforms to this protocol. Enums are used for classifying errors
+*/
+
+enum AwfulError: ErrorType {
+  case Bad
+  case Worse
+  case Terrible
+}
+class Error {}
+
+func doSomething() throws -> Error {
+  // If something bad happendd, throw the error
+  throw AwfulError.Bad
+
+  // if something worse happends, throw the error
+  throw AwfulError.Worse
+
+  // If something terrible happends, throw the error
+  throw AwfulError.Terrible
+
+  // make can return here
+  return Error()
 }
 
+/*:
+In order to catch errors, a new do-catch statement is available
+
+*/
+
+do {
+  let result = try doSomething()
+}
+catch AwfulError.Bad {}
+catch AwfulError.Worse {}
+catch AwfulError.Terrible {}
